2b5e438c51c89e1b39af01a4ea0ccf0c6509b784
Pavel Kosarev <81260917+pkosarev@users.noreply.github.com>
2022-11-21 21:57:10 +0300

    Update breakpoints.css.ts

--- styles/breakpoint.css.ts
+++ styles/breakpoint.css.ts
@@ -1,5 +1,5 @@
 const tableWidth = '768px';
-const sDesctopWidth = '1280px';
+const sDesktopWidth = '1280px';
 
 export const onTableWidth = `@media screen and (max-width: ${tableWidth})`;
-export const onSDesctopWidth = `@media screen and (max-width: ${sDesctopWidth})`;
+export const onSDesktopWidth = `@media screen and (max-width: ${sDesktopWidth})`;
